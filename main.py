6c15a8f3002de5bfb3da319ec2e0ebf4a460fe0b
daviaan <daviaan@daviaan.nl>
2025-06-27 15:20:45 +0200

    tokens added

--- main.py
+++ main.py
@@ -3,6 +3,17 @@
 
 from gpt4all import GPT4All
 
-model = GPT4All("gpt4all-13b-snoozy-q4_0.gguf", device='cpu')
-output = model.generate("The capital of Italy is ", max_tokens=3)
-print("The capital is : ",output)
+# model = GPT4All("gpt4all-13b-snoozy-q4_0.gguf", device='cpu', allow_download=True)
+# output = model.generate("The capital of Mongolia is ", max_tokens=10)
+# print("The capital is : ",output)
+
+model = GPT4All('Phi-3-mini-4k-instruct.Q4_0.gguf', device='cpu', allow_download=True)
+tokens = []
+with model.chat_session():
+    for token in model.generate("What is the capital of France?", streaming=True):
+        tokens.append(token)
+    print(tokens)
+    # output to console:
+    for token in model.generate("What is the highest mountain in South America?", streaming=True):
+        print(token, end='', flush=True)
+    print()
